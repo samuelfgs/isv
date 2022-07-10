902c55f7a7a101fa09c9020458914d05c75a409e
Samuel Santos <samuel@plasmic.app>
2022-07-10 02:40:09 -0300

    Add page title

--- pages/_app.js
+++ pages/_app.js
@@ -3,14 +3,18 @@ import '../styles/globals.css'
 import Script from 'next/script'
 import { PlasmicRootProvider } from "@plasmicapp/react-web";
 import PlasmicGlobalContextsProvider from "../components/plasmic/isv/PlasmicGlobalContextsProvider";
+import Head from "next/head";
 function MyApp({ Component, pageProps }) {
-  return (<>
+  return <>
+    <Head>
+      <title>Igreja em SV</title>
+    </Head>
     <PlasmicRootProvider>
       <PlasmicGlobalContextsProvider>
         <Component {...pageProps} />
       </PlasmicGlobalContextsProvider>
     </PlasmicRootProvider>
-  </>);
+  </>;
 }
 
 export default MyApp
