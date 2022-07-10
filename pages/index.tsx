80b089dff4c5e6b73a0114ae993cb5a34f8b4509
Samuel Santos <samuel@plasmic.app>
2022-07-10 11:54:40 -0300

    Use real product price

--- pages/index.tsx
+++ pages/index.tsx
@@ -42,7 +42,7 @@ function Homepage() {
         variantId: JSON.parse(item.variantId)
       }),
       title: item.product.fields.name,
-      unit_price: 0.4,
+      unit_price: item.product.fields.price,
       quantity: item.quantity
     }));
 
