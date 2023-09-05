5c8a4ba5d1aab3ce4fe62b13b596f141bfc7a0ff
Samuel <samuel@plasmic.app>
2023-09-05 13:37:28 -0300

    test

--- pages/api/webhook.ts
+++ pages/api/webhook.ts
@@ -6,6 +6,6 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  console.log("dale", "webhook", req.body);
+  console.log("dale", "webhook2", req.body);
   res.status(200).json(true)
 }
